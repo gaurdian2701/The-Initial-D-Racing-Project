d1f550071f9848211d85d94780a2a15b8e4f3578
Kiran George <gaurdian.kiran@gmail.com>
2026-01-21 14:47:43 +0100

    shit

--- Assets/Kiran/Kiran_Scripts/ArcadeyCarController.cs
+++ Assets/Kiran/Kiran_Scripts/ArcadeyCarController.cs
@@ -7,12 +7,12 @@ namespace Car
     {
         [SerializeField] private float mgripDuringLateralMovement = 1.0f;
         [SerializeField] private float mgripDuringSidewaysMovement = 4.0f;
-        void Update()
+        protected override void Update()
         {
             base.Update();
         }
 
-        void FixedUpdate()
+        protected override void FixedUpdate()
         {
             base.FixedUpdate();
         }
